1a0e42df48764ce04b03c73739d588fb3d7d1181
Erik Itter <erik.itter@gmail.com>
2024-09-16 17:09:00 +0200

    setup

--- m.ps1
+++ m.ps1
@@ -1,12 +1,28 @@
 
 $StopWatch = [System.Diagnostics.Stopwatch]::StartNew();
+$stepCounter = 1;
 
-node .\dia.js
-Write-Host ("time preparing LaTeX "+$StopWatch.Elapsed.ToString());
+Write-Host -ForegroundColor DarkGray ("`nStep "+$stepCounter+": preparing LaTeX.");
+$process = Start-Process -FilePath "node.exe" -WorkingDirectory "." -NoNewWindow -ArgumentList "./src/dia.mjs" -PassThru
+Wait-Process -InputObject $process
 
-Start-Process -FilePath "xelatex.exe" -WorkingDirectory "./latex" -NoNewWindow -ArgumentList "--interaction=batchmode --halt-on-error dia" -Wait
+if ($process.ExitCode -ne 0) {
+    Write-Warning "$_ exited with status code $($process.ExitCode)";
+    return $process.ExitCode;
+}
+Write-Host -ForegroundColor DarkGray ("time preparing LaTeX "+$StopWatch.Elapsed.ToString());
+$stepCounter++;
+
+Write-Host -ForegroundColor DarkGray ("`nStep "+$stepCounter+": running LaTeX.");
+$process = Start-Process -FilePath "xelatex.exe" -WorkingDirectory "./latex" -NoNewWindow -ArgumentList "--interaction=batchmode --halt-on-error dia" -PassThru
+Wait-Process -InputObject $process
+if ($process.ExitCode -ne 0) {
+    Write-Warning "$_ exited with status code $($process.ExitCode)";
+    return $process.ExitCode;
+}
+
 
 $StopWatch.Stop();
 
 
-Write-Host ("time total "+$StopWatch.Elapsed.ToString());
+Write-Host -ForegroundColor Green ("`nSuccess, time total "+$StopWatch.Elapsed.ToString());
